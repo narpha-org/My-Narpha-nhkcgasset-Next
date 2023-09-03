566847f7efc4eabbe60a47e9aba96e1c79fd7097
ARAKAKI Hiroyuki <arakaki.narpha.org@gmail.com>
2023-09-04 02:04:28 +0900

    update

--- ecr_push_mynarpha-next.sh
+++ ecr_push_mynarpha-next.sh
@@ -2,6 +2,6 @@
 
 GIT_COMMIT_ID=$(git log --format="%H" -n 1)
 aws ecr get-login-password --region ap-northeast-1 --profile AWS_Narpha_amplify | docker login --username AWS --password-stdin 735641424488.dkr.ecr.ap-northeast-1.amazonaws.com
-docker build -t mynarpha-next:"${GIT_COMMIT_ID}" -f Dockerfile.prod --env-file ./.env.production .
+docker build -t mynarpha-next:"${GIT_COMMIT_ID}" -f Dockerfile.prod .
 docker tag mynarpha-next:"${GIT_COMMIT_ID}" 735641424488.dkr.ecr.ap-northeast-1.amazonaws.com/mynarpha-next:"${GIT_COMMIT_ID}"
 docker push 735641424488.dkr.ecr.ap-northeast-1.amazonaws.com/mynarpha-next:"${GIT_COMMIT_ID}"
